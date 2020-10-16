25e81b4bd91877a0362a5095564b1428808910d3
UmairMukhtar <umairmukhtar27@gmail.com>
2020-10-17 00:00:10 +0500

    Added a new program

---
+++ C++ Counting till 10 using for loop.cpp
@@ -0,0 +1,12 @@
+//this simple program will display counting till 10 using for loop
+#include<iostream>
+using namespace std;
+int main()
+{
+	//start value of i from zero and keep increasing its value untill it is less than 10
+	for(int i=0; i<10; i++)
+	{
+		//As value of is i starting from 0 so we will add 1 to it everytime we display it
+		cout<<i+1<<endl;
+	}
+}
