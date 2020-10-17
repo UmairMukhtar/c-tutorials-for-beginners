dc47e2d9411a6f1631b7595a6fe4287246db4278
UmairMukhtar <umairmukhtar27@gmail.com>
2020-10-17 23:13:41 +0500

    Added if statements

---
+++ C++ Grade System using if Statements.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//decalre an integer variable  and give it any value
+	int grade = 30;
+	if(grade<30)
+	{
+		//this will be dispaly on if grade is less than thirty
+		cout<<"You have got an F grade"<<endl;
+	}
+	else if(grade>=30)
+	{
+		//this will be dispaly on if grade is greater than or equal to thirty
+		cout<<"You have got an A grade"<<endl;
+	}
+
+}
