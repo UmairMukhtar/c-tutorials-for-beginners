e83851eb3d7f0af79de48cfdb12627f07d0c0ae2
UmairMukhtar <umairmukhtar27@gmail.com>
2020-10-21 22:22:45 +0500

    more

---
+++ Arrays Basic.cpp
@@ -0,0 +1,10 @@
+//this simple program will display counting till 10 using while loop
+#include<iostream>
+using namespace std;
+int main()
+{
+	// here we are declaring an array of integer type
+	int i[5] ={1,2,3,4,5};
+	//Array index starts at zero so following statement will print first element of array
+	cout<<i[0];
+}
